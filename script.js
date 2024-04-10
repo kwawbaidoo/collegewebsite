44795e4b91f8745964752791d55b31dff7654bc9
Francis Baidoo <kwaw.baidoo@gmail.com>
2024-04-10 19:00:57 +0000

    modified various pages, style, script

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 const navLinks = document.querySelectorAll(".nav_link");
+const list = document.querySelectorAll(".navigation-list");
 const pathname = window.location.pathname;
 console.log(pathname);
+console.log(navLinks);
 
-navLinks.forEach(nav =>{
-    if(nav.href.includes(pathname)) {
-        nav.classList.add("active");
-    }
+const loading = document.getElementById("loading");
+const toggle = document.getElementById("toggleSection");
+const onLoadClick = loadPage();
+
+navLinks.forEach((nav) => {
+  if (nav.href.includes(pathname)) {
+    nav.classList.add("active");
+  }
 });
+
+
+function click(){
+    navLinks.forEach((nav)=>{
+        nav.add(setAttribute("onClick", "onLoadClick"));
+    })
+}
+
+function loadPage() {
+//   toggle.style.display = "none";
+  setInterval(  toggle.style.display = "none"
+  , 5000);
+  toggle.style.display = "block";
+}
